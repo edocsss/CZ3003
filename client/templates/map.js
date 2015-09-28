83708ff87455b253aefa986f3b9dc241fc655bf7
VotBear <vot1_bear@yahoo.com>
2015-09-28 19:42:18 +0800

    Update form (Submit still error) (help)
    pls

--- client/templates/map.js
+++ client/templates/map.js
@@ -6,7 +6,7 @@ var contentString;
 
 var infowindow;
 
-var marker;
+var marker; 
 
 Template.map.onCreated(function () {
 	GoogleMaps.ready('map', function (map) {
@@ -35,27 +35,59 @@ Template.map.onCreated(function () {
 			});
 
 			contentString =  
-					'<h5 id="firstHeading" class="firstHeading">New Case&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</h3>'+
-					'<div id="bodyContent">'+
-					'<form class="new-task">' +
-					//'Title:<br>' +
-        			'<input type="text" name="title" placeholder="Title" /><br><br>'+
-					'Case Type:<br>' +
-        			'<select name="type" width="200">' +
-					 	'<option value="opt1">Fire</option>'+
-					  	'<option value="opt2">Bigger Fire</option>'+
-					'</select><br><br>'+
-					'Address:<br>' +
-        			'<input type="text" name="address" placeholder="Address" /><br><br>'+
-					'Description:<br>' +
-					'<textarea name="description">...</textarea><br><br>' +
-					'<input type="reset" value="Reset"> <input type="submit" value="Submit">' +
-      				'</form>'+
-					'</div>';
+				'<h5 id="firstHeading" class="text-center">Create New Case</h5>'+
+
+				'<div class="container-fluid">'+
+					'<form class="form-horizontal center-block" id ="create-case-form">' +
+					
+						'<div class="form-group">'+
+							'<div class="col-sm-12">'+
+								'<input type="text" class="form-control" name="title" placeholder="Title" id ="create-case-title">'+
+							'</div>'+
+						'</div>'+
+
+
+						'Case Type:' +
+
+						'<div class="form-group">'+
+							'<div class="col-sm-12">'+
+				    			'<select class="form-control formwidth" name="type" width="200" id ="create-case-type">' +
+								 	'<option value="opt1">Fire</option>'+
+								  	'<option value="opt2">Bigger Fire</option>'+
+								'</select>'+
+							'</div>'+
+						'</div>'+
+						
+						'Address:' +
+
+						'<div class="form-group">'+
+							'<div class="col-sm-12">'+
+								'<input type="text" class="form-control" name="address" placeholder="Address" id ="create-case-address"/>'+
+							'</div>'+
+						'</div>'+
+						
+						'Description:' +
+
+						'<div class="form-group">'+
+							'<div class="col-sm-12">'+
+								'<textarea class="form-control formwidth" name="description" id ="create-case-description"></textarea>' +
+							'</div>'+
+						'</div>'+
+						
+						'<div class="form-group">'+
+							'<div class="col-sm-5">'+
+								'<input type="reset"  class="btn btn-primary left-block" value="Reset">'+ 
+							'</div>'+
+							'<div class="col-sm-6">'+ 
+								'<input type="submit" class="btn btn-primary left-block" value="Submit">' +
+							'</div>'+
+						'</div>'+
+						
+					'</form>'+
+				'</div>' ;
 
 			infowindow = new google.maps.InfoWindow({ 
-				content: contentString,
-				minWidth: 700, 
+				content: contentString
 			});
 
 			marker.addListener('click', function() {
@@ -63,6 +95,104 @@ Template.map.onCreated(function () {
 			});
 		}
 	}
+});
+
+Template.map.onRendered(function () {
+
+
+			var createCaseValidator = $('#create-case-form').validate({
+				submitHandler: function (form, event) {
+
+					console.log ("lolbefore");
+					event.preventDefault();
+					console.log ("lol");
+
+					var title 		= $('#create-case-title').val();
+					var type 		= $('#create-case-type').val();
+					var address 	= $('#create-case-address').val();
+					var description = $('#create-case-description').val();
+
+					return false;
+					/*
+					Meteor.createCaseWithPassword(email, password, function (error) {
+						if (error) {
+							if (error.reason == "User not found") {
+								createCaseValidator.showErrors({
+									email: error.reason
+								});
+							}
+
+							if (error.reason == "Incorrect password") {
+								createCaseValidator.showErrors({
+									password: error.reason
+								});
+							}
+
+							if (error.reason != "User not found" & error.reason != "Incorrect password") {
+								swal('createCase', error.reason, 'error');
+							}
+						}
+					});*/
+				},
+				rules: {
+					title: {
+						minlength: 3,
+						maxlength: 20,
+						required: true
+					},
+					type: {
+						required: true
+					},
+					address: {
+						minlength: 3,
+						maxlength: 40,
+						required: true
+					},
+					description: {
+						minlength: 3,
+						maxlength: 200,
+						required: true
+					}
+				},
+
+				messages: {
+					title: {
+						minlength: "The case title must be between 3 to 20 characters!",
+						maxlength: "The case title must be between 3 to 20 characters!",
+						required: "You must enter a case title!"
+					},
+					type: {
+						required: "You must select a case type!"
+					}, 
+					address: {
+						minlength: "The address must be between 3 to 40 characters!",
+						maxlength: "The address must be between 3 to 40 characters!",
+						required: "You must enter an address!"
+					}, 
+					description: {
+						minlength: "The description must be between 3 to 200 characters!",
+						maxlength: "The description must be between 3 to 200 characters!",
+						required: "You must enter a description!"
+					}
+				},
+				highlight: function (element) {
+					$(element).closest('.form-group').addClass('has-error');
+					$(element).closest('.form-group').removeClass('has-success');
+				},
+				unhighlight: function (element) {
+					$(element).closest('.form-group').addClass('has-success');
+					$(element).closest('.form-group').removeClass('has-error');
+				},
+				errorElement: 'span',
+				errorClass: 'help-block',
+				errorPlacement: function (error, element) {
+					if (element.parent('.input-group').length) {
+						error.insertAfter(element.parent());
+					} else {
+						error.insertAfter(element);
+					}
+				}
+			});
 });
 
 Template.map.helpers({
@@ -78,10 +208,12 @@ Template.map.helpers({
 });
 
 
+
 Template.map.events({
 	"submit .new-task": function (event) {
-		// Prevent default browser form submit
+		// Prevent default browser form submit 
 		event.preventDefault();
+
 
 		// Get value from form element
 		//TODO: Input check, make sure all is filled
@@ -100,6 +232,20 @@ Template.map.events({
 	}
 });
 
+
+/*
+
+	TODO
+
+	Benerin form --> ikutin Bootstrap classes (see resetPasswordForm for example) V
+	Form checking --> use jQuery validator (see other JS files loginForm.js)
+	create insert method @ methods.js
+	Marker --> different color and undragable for existing cases
+	Load existing cases marker initially
+	Observe realtime cases (see below)
+	Kalau ada user lgsg approve, else pending + severity NULL
+
+*/
 /*	reference
 
 var markers = {};   
